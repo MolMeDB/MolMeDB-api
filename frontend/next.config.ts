b013a4be4eaa33ce1fae6c1c5146477627719129
Jakub Juračka <44319136+jurja00@users.noreply.github.com>
2025-08-13 16:42:17 +0200

    Update: Frontend standalone build output

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ["http://localhost*", "192.168.0.*"],
+    /* config options here */
+    allowedDevOrigins: ["http://localhost*", "192.168.0.*"],
+    output: "standalone",
+    // swcMinify: true,
+    // reactStrictMode: true,
 };
 
 export default nextConfig;
