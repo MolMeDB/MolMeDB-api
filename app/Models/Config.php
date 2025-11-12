a12c5e504db8c5a30b49002dc0ff44643dba445d
Jakub Juračka <44319136+jurja00@users.noreply.github.com>
2025-11-12 07:09:56 +0100

    Add: Config model getter/setter

--- app/Models/Config.php
+++ app/Models/Config.php
@@ -9,4 +9,26 @@ class Config extends Model
 {
     /** @use HasFactory<\Database\Factories\ConfigFactory> */
     use HasFactory;
+
+    protected $primaryKey = 'key';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+
+    public static function get($key, $default = null)
+    {
+        return static::where('key', $key)->value('value') ?? $default;
+    }
+
+    public static function set($key, $value)
+    {
+        return static::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+    }
 }
