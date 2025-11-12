0cfaf3de678d08491c39757615352538ba878789
Jakub Juračka <44319136+jurja00@users.noreply.github.com>
2025-11-12 07:02:15 +0100

    Add: Command - Join structure records

---
+++ app/Console/Commands/JoinStructureRecords.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Modules\Rdkit\Rdkit;
+
+class JoinStructureRecords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'structures:join-records {id1} {id2}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Takes two structure IDs and joins the second into the first one, reassigning all related data.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Checking structures...');
+
+        $substance_1 = \App\Models\Structure::where('id', $this->argument('id1'))->first();
+        $substance_2 = \App\Models\Structure::where('id', $this->argument('id2'))->first();
+
+        if (!$substance_1 || !$substance_2) {
+            $this->error('One of the structures does not exist.');
+            return;
+        }
+
+        $rdkit = new Rdkit();
+
+        if(!$rdkit->is_connected())
+        {
+            $this->error('Rdkit is not connected. Stopping...');
+            return;
+        }
+           
+        $smiles_1 = $rdkit->canonize_smiles($substance_1->canonical_smiles);
+        $smiles_2 = $rdkit->canonize_smiles($substance_2->canonical_smiles);
+
+        if(!$smiles_1 || !$smiles_2)
+        {
+            $this->error('Could not canonize one of the structures. Stopping...');
+            return;
+        }
+
+        if ($smiles_1 != $smiles_2) {
+            $this->error('The structures have different SMILES.');
+            return;
+        }
+
+        \App\Models\Structure::join_structures($substance_1, $substance_2);
+
+        $this->info('Done.');
+    }
+}
