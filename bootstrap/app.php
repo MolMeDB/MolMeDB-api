17e269101364e38d027d69cbcb4ef8ee6aca5e51
Jakub Juračka <jakubjuracka@upol.cz>
2026-06-30 19:41:16 +0200

    File signature fix?

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureEmailIsVerified;
+use App\Http\Middleware\NormalizeLivewireUploadUrl;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -35,6 +36,9 @@ return Application::configure(basePath: dirname(__DIR__))
         );
         $middleware->api(prepend: [
             EnsureFrontendRequestsAreStateful::class,
+        ]);
+        $middleware->web(prepend: [
+            NormalizeLivewireUploadUrl::class,
         ]);
 
         $middleware->alias([
